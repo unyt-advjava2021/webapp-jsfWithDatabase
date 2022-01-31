b2efe307fe980f106a133d2e5675bbee14569136
Elton Ballhysa <elton.ballhysa@hititcs.com>
2022-01-31 14:19:57 +0100

    python client removed (unintentional commit)

--- python/client.py
+++
@@ -1,9 +0,0 @@
-import requests
-from dict2xml import dict2xml
-
-data = {'departmentCode': 'EE'}
-xml_data = dict2xml(data)
-headers = {'Content-Type': 'text/xml'}
-res = requests.post('http://localhost:8080/XMLServlet/courses', data=xml_data, headers=headers)
-print(f'Response status code is {res.status_code}' )
-print(res.text)
